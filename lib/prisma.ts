6550fd035a25c2468923d2f22fdde5dbebc61b50
anondotli <hi@anon.li>
2026-04-25 00:30:15 +0200

    Migrate Prisma, update packages.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,20 +1,31 @@
 // lib/prisma.ts
-import { PrismaClient } from "@prisma/client"
-import { withAccelerate } from "@prisma/extension-accelerate"
+import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const prismaClientSingleton = () => {
-    return new PrismaClient({
-        accelerateUrl: process.env.DATABASE_URL,
-    }).$extends(withAccelerate())
-}
+  const DATABASE_URL = process.env.DATABASE_URL;
+
+  if (!DATABASE_URL) {
+    throw new Error("DATABASE_URL is not set");
+  }
+
+  const adapter = new PrismaPg({
+    connectionString: DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  return new PrismaClient({ adapter });
+};
 
 declare global {
-     
-    var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+  // eslint-disable-next-line no-var
+  var prisma: undefined | ReturnType<typeof prismaClientSingleton>;
 }
 
-export const prisma = globalThis.prisma ?? prismaClientSingleton()
+export const prisma = globalThis.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== 'production') {
-    globalThis.prisma = prisma
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prisma = prisma;
 }
